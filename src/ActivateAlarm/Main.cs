55b9a33262da631f9bfbc1e5d92c4b806202e607
LondonStudios <64035280+LondonStudios@users.noreply.github.com>
2020-05-02 15:43:02 +0100

    Added source code

---
+++ src/ActivateAlarm/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using static CitizenFX.Core.Native.API;
+
+namespace ActivateAlarm
+{
+    public class Main : BaseScript
+    {
+        private bool AlarmStatus;
+
+        [Command("alarm")]
+        private void AlarmController()
+        {
+            AlarmStatus = !AlarmStatus;
+            Vector3 position = Game.Player.Character.Position;
+            var status = "";
+            if (AlarmStatus == true)
+            {
+                status = "~g~Activated";
+            }
+            else
+            {
+                if (AlarmStatus == false)
+                    status = "~b~Deactivated";
+            }
+            Screen.ShowNotification("~r~Alarm Activation~s~:\nStatus: " + status);
+            SoundAlarm(position);
+        }
+
+        private async void SoundAlarm(Vector3 position)
+        {
+            while (AlarmStatus == true)
+            {
+                TriggerServerEvent("Server:SoundToCoords", position.X, position.Y, position.Z, 30.0f, "alarm", 1.0f);
+                await Delay(3800);
+            }
+            
+
+        }
+
+        /*© 2020 - London Studios - Do not redistibute, modify/change or reupload without my obtained permission. 
+         * This may be used on public/private FiveM servers and used in videos published to websites, however the source files should not be redistributed. 
+         * This is for non-commercial use.*/
+    }
+}
---
+++ src/ActivateAlarm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ActivateAlarm")]
+[assembly: AssemblyDescription("A FiveM resource used to activate a custom alarm sound in an area coded in C#.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("London Studios")]
+[assembly: AssemblyProduct("ActivateAlarm")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b0942a8-0e73-49f0-95c1-fa57d1e8c449")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
